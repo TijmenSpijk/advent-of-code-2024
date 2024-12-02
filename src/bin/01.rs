76505200476eb104c4626b53dab3154d3143a0fc
Tijmen <tijmenspijk@gmail.com>
2024-12-02 22:25:40 +0100

    empty lines in lines() output for some reason

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,7 +1,22 @@
+use log::info;
+
 advent_of_code::solution!(1);
 
 pub fn part_one(input: &str) -> Option<u32> {
-    None
+    let lines = input.lines();
+    let mut result: u32 = 0;
+    for line in lines {
+        let values = line.split(' ');
+        let mut difference: i32 = 0;
+        let mut counter: i32 = 1;
+        for value in values {
+            println!("parsing {value}");
+            // difference = difference + counter * value.parse::<i32>().expect("error");
+            counter = counter * -1;
+        }
+        result = result + difference.abs() as u32;
+    };
+    Some(result)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
