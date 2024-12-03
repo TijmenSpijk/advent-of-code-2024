8cc2af0adc43850668942030aef013039e1ae2e6
Tijmen <tijmenspijk@gmail.com>
2024-12-03 21:13:09 +0100

    day 2 part 1

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -4,24 +4,7 @@ pub fn part_one(input: &str) -> Option<u32> {
     let mut result = 0;
 
     for line in input.lines() {
-        let values = line.split_whitespace().map(|a| a.parse::<u32>().unwrap());
-        let mut dup = values.clone();
-        dup.next();
-        let combo = values.zip(dup);
-
-        let increasing = combo.clone().fold(true, |a , b| {
-            a && b.0 > b.1
-        });
-    
-        let decreasing = combo.clone().fold(true, |a , b| {
-            a && b.0 < b.1
-        });
-    
-        let difference = combo.clone().fold(true, |a , b| {
-            a && b.0.abs_diff(b.1) <= 3
-        });
-
-        if (increasing || decreasing) && difference {
+        if check_safety(line) {
             result += 1;
         }
     }
@@ -33,29 +16,33 @@ pub fn part_two(input: &str) -> Option<u32> {
     let mut result = 0;
 
     for line in input.lines() {
-        let values = line.split_whitespace().map(|a| a.parse::<u32>().unwrap());
-        let mut dup = values.clone();
-        dup.next();
-        let combo = values.zip(dup);
-
-        let increasing = combo.clone().fold(true, |a , b| {
-            a && b.0 > b.1
-        });
-    
-        let decreasing = combo.clone().fold(true, |a , b| {
-            a && b.0 < b.1
-        });
-    
-        let difference = combo.clone().fold(true, |a , b| {
-            a && b.0.abs_diff(b.1) <= 3
-        });
-
-        if (increasing || decreasing) && difference {
+        if check_safety(line) {
             result += 1;
         }
     }
 
     Some(result)
+}
+
+fn check_safety(line: &str) -> bool {
+    let values = line.split_whitespace().map(|a| a.parse::<u32>().unwrap());
+    let mut dup = values.clone();
+    dup.next();
+    let combo = values.zip(dup);
+
+    let increasing = combo.clone().fold(true, |a , b| {
+        a && b.0 > b.1
+    });
+
+    let decreasing = combo.clone().fold(true, |a , b| {
+        a && b.0 < b.1
+    });
+
+    let difference = combo.clone().fold(true, |a , b| {
+        a && b.0.abs_diff(b.1) <= 3
+    });
+
+    (increasing || decreasing) && difference
 }
 
 #[cfg(test)]
